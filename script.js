1ecbe5e343564573211571ea8ad4d1c0224e96e1
SrtMiah <miriamlopes99@gmail.com>
2025-06-06 17:23:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showLove() {
+    const hearts = document.getElementById("hearts");
+    for (let i = 0; i < 10; i++) {
+        let heart = document.createElement("span");
+        heart.innerHTML = "❤️";
+        heart.style.position = "absolute";
+        heart.style.left = `${Math.random() * 100}%`;
+        heart.style.top = `${Math.random() * 100}%`;
+        hearts.appendChild(heart);
+    }
+}
