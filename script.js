15fee64bae2811619d4c2e222f01a45d69fc6922
SrtMiah <miriamlopes99@gmail.com>
2025-06-06 17:33:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.getElementById("openCardBtn").addEventListener("click", function() {
+    document.getElementById("card").style.display = "block"; // Exibe o cartão
+    this.style.display = "none"; // Oculta o botão de abrir
+});
+
 function showLove() {
     const hearts = document.getElementById("hearts");
     for (let i = 0; i < 10; i++) {
